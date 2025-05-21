0e41b437605284ff72d4ba8cbbe7cccad3e3abff
jonnathans06 <jhonasaav@gmail.com>
2025-05-21 08:41:52 -0500

    Cambios

--- src/main/ejercicio2/RespuestasOptimizadas.java
+++ src/main/ejercicio2/RespuestasOptimizadas.java
@@ -8,23 +8,43 @@ public class RespuestasOptimizadas {
     public static String tieneDuplicadosComplejidad = ""; // Ej: "O(n)"
 
     public static boolean tieneDuplicadosOpt(int[] arr) {
-        // TODO: implementar versión eficiente
+        int max = arr.length;
+        boolean[] duplicado = new boolean[max];
+
+        for (int num : arr){
+            if(duplicado[num]) return true;
+            duplicado[num] = true;
+        }
         return false;
     }
 
     // 2. Versión optimizada de contarMayores (Complejidad anotada en la variable)
     public static String contarMayoresComplejidad = ""; // Ej: "O(n)"
-
+  
     public static int contarMayoresOpt(int[] arr, int x) {
-        // TODO: implementar versión eficiente
-        return 0;
+            int contador = 0;
+            for (int num : arr){
+                if(num > x) {
+                    contador++;
+                }
+            }
+        return contador;
     }
 
     // 3. Versión optimizada de encontrarMaximo (Complejidad anotada en la variable)
     public static String encontrarMaximoComplejidad = ""; // Ej: "O(n)"
 
-    public static int encontrarMaximoOpt(int[] arr) {
-        // TODO: implementar versión eficiente
-        return 0;
+   public static int encontrarMaximoOpt(int[] arr) {
+        if (arr.length == 0) {
+            throw new IllegalArgumentException("El arreglo no debe estar vacío");
+        }
+
+        int maximo = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maximo) {
+                maximo = arr[i];
+            }
+        }
+        return maximo;
     }
 }
